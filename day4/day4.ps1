625ed260659bffe81b5165ce640d62bc44d8a943
lasse.hastrup <lasse.hastrup@fellowmind.dk>
2024-12-04 10:05:46 +0100

    Updated with part2

--- day4/day4.ps1
+++ day4/day4.ps1
@@ -2,6 +2,9 @@ function Count-Xmas {
     param (
         [string[]]$grid
     )
+    if ($null -eq $grid -or $grid.Length -eq 0) {
+        throw "Grid is empty or null"
+    }
 
     # Define the word to search for and its length
     $word = "XMAS"
@@ -9,13 +12,13 @@ function Count-Xmas {
 
     # Define all possible directions to search for the word
     $directions = @(
-        @(0, 1),  # right
-        @(1, 0),  # down
-        @(1, 1),  # down-right
+        @(0, 1), # right
+        @(1, 0), # down
+        @(1, 1), # down-right
         @(1, -1), # down-left
         @(0, -1), # left
         @(-1, 0), # up
-        @(-1, -1),# up-left
+        @(-1, -1), # up-left
         @(-1, 1)  # up-right
     )
 
@@ -67,7 +70,50 @@ function Count-Xmas {
 }
 
 # Read grid from input.txt
-$grid = Get-Content -Path "input.txt"
+$grid = Get-Content -Path "./day4/input.txt"
+if ($null -eq $grid -or $grid.Length -eq 0) {
+    throw "Grid is empty or null"
+}
 
 # Print the count of occurrences of the word "XMAS"
 Write-Output (Count-Xmas -grid $grid)
+
+## Part 2
+
+function Find-XMAS {
+    param (
+        [string[]]$grid
+    )
+    $count = 0
+    for ($i = 0; $i -lt $grid.Length - 2; $i++) {
+        for ($j = 0; $j -lt $grid[$i].Length - 2; $j++) {
+            # Check for X-MAS pattern in all directions
+            if (($grid[$i][$j] -eq 'M' -and $grid[$i + 2][$j] -eq 'M') -and
+                ($grid[$i + 1][$j + 1] -eq 'A') -and
+                ($grid[$i][$j + 2] -eq 'S' -and $grid[$i + 2][$j + 2] -eq 'S')) {
+                $count++
+            }
+            if (($grid[$i][$j + 2] -eq 'M' -and $grid[$i + 2][$j + 2] -eq 'M') -and
+                ($grid[$i + 1][$j + 1] -eq 'A') -and
+                ($grid[$i][$j] -eq 'S' -and $grid[$i + 2][$j] -eq 'S')) {
+                $count++
+            }
+            if (($grid[$i][$j] -eq 'M' -and $grid[$i][$j + 2] -eq 'M') -and
+                ($grid[$i + 1][$j + 1] -eq 'A') -and
+                ($grid[$i + 2][$j] -eq 'S' -and $grid[$i + 2][$j + 2] -eq 'S')) {
+                $count++
+            }
+            if (($grid[$i + 2][$j] -eq 'M' -and $grid[$i + 2][$j + 2] -eq 'M') -and
+                ($grid[$i + 1][$j + 1] -eq 'A') -and
+                ($grid[$i][$j] -eq 'S' -and $grid[$i][$j + 2] -eq 'S')) {
+                $count++
+            }
+        }
+    }
+    return $count
+}
+
+$grid = $data -split "`n"
+$result = Find-XMAS -grid $grid
+Write-Output $result
+
